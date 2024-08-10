879a0e1238bc821c2994b05819ef4dd89d81269f
Mukesh Jaiswal <jais.mukesh@gmail.com>
2024-08-10 22:35:48 +0200

    Add url, view & template

--- backend/lab/views.py
+++ backend/lab/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    # return HttpResponse("Moonbase Lab Home Page!")
+    return render(request, 'home.html')
