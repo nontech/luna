77d062b463f0ff8dd5104bd952b2e21e0e36bef6
Mukesh Jaiswal <jais.mukesh@gmail.com>
2024-08-12 01:43:12 +0200

    Add remaining tables based on ERD diagram

--- backend/lab/models.py
+++ backend/lab/models.py
@@ -8,8 +8,84 @@ class Users(models.Model):
     username = models.CharField(max_length=50)
     email = models.EmailField()
     password = models.CharField(max_length=50)
+
+    # timestamps
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'  # This overrides the default table name
+
+class Classrooms(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    creator_user = models.ForeignKey(Users, on_delete=models.CASCADE)
+
+    # timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'classrooms'  # This overrides the default table name
+
+class ClassroomUsers(models.Model):
+    classroom = models.ForeignKey(Classrooms, on_delete=models.CASCADE)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+
+    # timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'classroom_users'  # This overrides the default table name
+
+class Exercises(models.Model):
+    name = models.CharField(max_length=50)
+    instructions = models.TextField()
+    output_instructions = models.TextField()
+    code = models.TextField()
+    creator_user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    test = models.OneToOneField('Tests', on_delete=models.CASCADE, null=True, blank=True)
+
+    # timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'exercises'  # This overrides the default table name
+
+class Tests(models.Model):
+    category = models.CharField(max_length=50)
+    output = models.CharField(max_length=50)
+    exercise = models.OneToOneField(Exercises, on_delete=models.CASCADE)
+
+    # timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'tests'  # This overrides the default table name
+
+class ClassroomExercises(models.Model):
+    classroom = models.ForeignKey(Classrooms, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercises, on_delete=models.CASCADE)
+
+    # timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'classroom_exercises'  # This overrides the default table name
+
+class UserExercises(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercises, on_delete=models.CASCADE)
+    status = models.CharField(max_length=50)
+    feedback = models.TextField()
+
+    # timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'user_exercises'  # This overrides the default table name
