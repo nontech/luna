5be4935e38ab07be050f58723bd954f7f5f709e3
Mukesh Jaiswal <jais.mukesh@gmail.com>
2024-08-11 21:55:08 +0200

    Add users table

--- backend/lab/models.py
+++ backend/lab/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Users(models.Model):
+    full_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'users'  # This overrides the default table name
