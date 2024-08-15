620fe2e1b20e1a4aad58c8342af2a4867cf2ef5e
Mukesh Jaiswal <jais.mukesh@gmail.com>
2024-08-15 12:42:01 +0200

    Register all models to admin

--- backend/lab/admin.py
+++ backend/lab/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Users, Classrooms, ClassroomUsers, Exercises, Tests, ClassroomExercises, UserExercises
+
+admin.site.register(Users)
+admin.site.register(Classrooms)
+admin.site.register(ClassroomUsers)
+admin.site.register(Exercises)
+admin.site.register(Tests)
+admin.site.register(ClassroomExercises)
+admin.site.register(UserExercises)
