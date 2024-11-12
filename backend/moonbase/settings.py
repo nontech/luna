f979ae3b2341fd040470151b1a26710ec66f4325
Mukesh Jaiswal <jais.mukesh@gmail.com>
2024-11-13 00:09:17 +0100

    Allow all CORS origin in backend

--- backend/moonbase/settings.py
+++ backend/moonbase/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +49,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+# During development
+CORS_ALLOW_ALL_ORIGINS = True  # Only for development!
+
+# For production, specify allowed origins:
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+# ]
 
 ROOT_URLCONF = 'moonbase.urls'
 
