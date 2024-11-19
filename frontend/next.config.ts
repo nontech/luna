eed3243548d7812f68dd8d14afd2a435c02bdff7
Mukesh Jaiswal <jais.mukesh@gmail.com>
2024-11-19 12:39:23 +0100

    Frontend back to using recommended .env files & default built-in way of handling environment variables. Not using APP_ENV as it clashes with default settings.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,20 +1,7 @@
 import type { NextConfig } from "next";
 
-const loadEnvConfig = () => {
-  switch (process.env.APP_ENV) {
-    case "docker":
-      return { envFilePath: ".env.docker" };
-    case "test":
-      return { envFilePath: ".env.test" };
-    case "production":
-      return { envFilePath: ".env.production" };
-    default:
-      return { envFilePath: ".env.local" };
-  }
-};
-
 const nextConfig: NextConfig = {
-  ...loadEnvConfig(),
+  // Add any other Next.js config options here
 };
 
 export default nextConfig;
