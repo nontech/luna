77a7a4782338995c63132807b05067ed48df921f
Mukesh Jaiswal <jais.mukesh@gmail.com>
2025-01-25 20:13:10 +0100

    Show ExerciseTests in Django admin

--- backend/lab/admin.py
+++ backend/lab/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Classrooms, ClassroomUsers, Exercises, Tests, ClassroomExercises, Submissions
+from .models import Classrooms, ClassroomUsers, Exercises, Tests, ClassroomExercises, Submissions, ExerciseTests
 
 admin.site.register(Classrooms)
 admin.site.register(ClassroomUsers)
@@ -9,3 +9,4 @@ admin.site.register(Exercises)
 admin.site.register(Tests)
 admin.site.register(ClassroomExercises)
 admin.site.register(Submissions)
+admin.site.register(ExerciseTests)
